3f4cab085165c9c7c3098e131cbd6c5c9e093ca9
zyma <61933199+imhexagram@users.noreply.github.com>
2024-01-03 21:09:55 +0000

    Add files via upload

--- bluetooth.c
+++ bluetooth.c
@@ -16,26 +16,43 @@ int main(){
         exit(1);
     }
     */
-
+    int status;
     struct sockaddr_rc loc_addr = {0}, rem_addr = {0};
     char buff[1024] = {0};
     int server = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
     loc_addr.rc_family = AF_BLUETOOTH;
-    //loc_addr.rc_bdaddr = ;
-    char mac[18]=MAC;
-    str2ba(mac, &loc_addr.rc_bdaddr);
-    loc_addr.rc_channel = (uint8_t)PORT;
+    loc_addr.rc_bdaddr = *BDADDR_ANY;
+    //char mac[18]=MAC;
+    //str2ba(mac, &loc_addr.rc_bdaddr);
+    loc_addr.rc_channel = 20;
+    //loc_addr.rc_channel = (uint8_t) 1;
 
-    bind(server, (struct sockaddr*)&loc_addr, sizeof(loc_addr));
+    status = bind(server, (struct sockaddr*)&loc_addr, sizeof(loc_addr));
+    if (status < 0) {
+        fprintf(stderr, "-- socket binding failed!\n");
+        exit(-1);
+    }
 
     ba2str(&loc_addr.rc_bdaddr, buff);
     fprintf(stdout, "local %s\n", buff);
 
-    listen(server,1);
+    status = listen(server,1);
+    if (status < 0) {
+        fprintf(stderr, "-- socket binding failed!\n");
+        exit(-1);
+    }
     printf("Waiting for connection...\n");
     //while(1){
     socklen_t opt = sizeof(rem_addr);
     int client = accept(server, (struct sockaddr*)&rem_addr, &opt);
+
+    if (client < 0) {
+        fprintf(stderr, "-- failed to connect to client\n");
+        exit(-1);
+    } else {
+        printf("-- client connected successfully\n");
+    }
+
     ba2str(&rem_addr.rc_bdaddr, buff);
     fprintf(stdout, "Connected to %s\n", buff);
 
