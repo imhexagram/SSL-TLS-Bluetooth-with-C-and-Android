86da278b5ae261431084015aff720d366a4ecc16
zyma <61933199+imhexagram@users.noreply.github.com>
2024-01-07 19:25:56 +0000

    Update and rename client2.c to socket_client.c

--- socket_client.c
+++ socket_client.c
@@ -14,20 +14,17 @@ int main() {
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
 
-    // 创建客户端套接字
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1) {
         perror("Error creating client socket");
         exit(EXIT_FAILURE);
     }
 
-    // 设置服务器地址结构
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-    // 连接到服务器
     if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("Error connecting to server");
         exit(EXIT_FAILURE);
@@ -35,19 +32,15 @@ int main() {
 
     printf("Connected to server\n");
 
-    // 发送数据给服务器并接收响应
     while (1) {
         printf("Enter message to send to server (type 'exit' to end): ");
         fgets(buffer, sizeof(buffer), stdin);
-
-        // 发送数据给服务器
         send(client_socket, buffer, strlen(buffer), 0);
 
         if (strcmp(buffer, "exit\n") == 0) {
             break;
         }
 
-        // 接收服务器的响应
         memset(buffer, 0, sizeof(buffer));
         ssize_t bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
 
@@ -59,7 +52,6 @@ int main() {
         printf("Received from server: %s", buffer);
     }
 
-    // 关闭套接字
     close(client_socket);
 
     return 0;
